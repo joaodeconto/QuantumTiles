1a2e576434009fa06503c1f72f6eb3e3cbc54441
João de Conto <joaovicentedeconto@gmail.com>
2024-11-12 20:56:57 -0300

    Android MVP / Input System
    Fix - Input System, ignore UI
    
    Android MVP

--- Assets/QuantumTiles/Scripts/InputManager.cs
+++ Assets/QuantumTiles/Scripts/InputManager.cs
@@ -1,16 +1,20 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class InputManager : MonoBehaviour
 {
     private GameInputActions gameInputActions;
+    [SerializeField] private GraphicRaycaster graphicRaycaster;
+    [SerializeField] private EventSystem eventSystem;
 
     private void Awake()
     {
         gameInputActions = new GameInputActions();
 
-        // Subscribe to the SelectCard action
+        // Subscribe to the SelectCard action with Modifier for click and position
         gameInputActions.Player.SelectCard.performed += OnSelectCard;
     }
 
@@ -26,22 +30,17 @@ public class InputManager : MonoBehaviour
 
     private void OnSelectCard(InputAction.CallbackContext context)
     {
-        // Use Invoke to delay the call to ProcessCardSelection slightly
-        Invoke(nameof(ProcessCardSelection), 0.01f);
-    }
-
-    private void ProcessCardSelection()
-    {
         // Check if card flipping is allowed in the GameManager
         if (!GameManager.Instance.CanSelectCard()) return;
 
-        // Check if the pointer is currently over any UI element
-        if (IsPointerOverUI()) return;
+        // Check if the pointer is over any UI element using GraphicRaycaster
+        if (IsPointerOverUI(context.ReadValue<Vector2>())) return;
 
-        // Raycast from the mouse position (or touch position)
-        Vector2 screenPosition = Mouse.current.position.ReadValue();
+        // Get the screen position from the SelectCard action's Modifier Composite
+        Vector2 screenPosition = context.ReadValue<Vector2>();
+
+        // Perform a raycast from the screen position
         Ray ray = Camera.main.ScreenPointToRay(screenPosition);
-
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             CardController card = hit.collider.GetComponent<CardController>();
@@ -54,9 +53,25 @@ public class InputManager : MonoBehaviour
         }
     }
 
-    private bool IsPointerOverUI()
+    private bool IsPointerOverUI(Vector2 position)
     {
-        // Returns true if the pointer is over any UI element
-        return EventSystem.current.IsPointerOverGameObject();
+        PointerEventData pointerData = new PointerEventData(eventSystem)
+        {
+            position = position
+        };
+
+        List<RaycastResult> raycastResults = new List<RaycastResult>();
+        graphicRaycaster.Raycast(pointerData, raycastResults);
+
+        // Loop through raycast results and return true only if we hit a UI element other than the Canvas
+        foreach (RaycastResult result in raycastResults)
+        {
+            if (result.gameObject != graphicRaycaster.gameObject) // Ignore the Canvas itself
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
